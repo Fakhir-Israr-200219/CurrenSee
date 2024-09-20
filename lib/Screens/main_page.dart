e5474c383daf0e6bedd43d6beb7e8723327cd44b
Fakhir-Israr-200219 <hmfakhirisrar@gmail.com>
2024-09-19 22:08:27 -0700

    buttom navigation implemantation Done

---
+++ lib/Screens/main_page.dart
@@ -0,0 +1,67 @@
+import 'package:currency_converter_app/Screens/Converter_Screen.dart';
+import 'package:currency_converter_app/Screens/CurrencyList.dart';
+import 'package:currency_converter_app/Screens/Login_screen.dart';
+import 'package:currency_converter_app/Screens/SingIn_screen.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  final List<Widget> pages = [
+    const ConverterScreen(),
+    const CurrencyList(),
+    const LoginScreen(),
+    const SingIn(),
+  ];
+  int currentPage = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[currentPage],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (value) {
+          setState(() {
+            currentPage = value;
+          });
+        },
+        currentIndex: currentPage,
+        items: [
+          BottomNavigationBarItem(
+              backgroundColor: const Color.fromARGB(255, 15, 171, 255),
+              icon: Icon(
+                color: Colors.black26,
+                Icons.monetization_on,
+              ),
+              label: "Converter"),
+          BottomNavigationBarItem(
+              backgroundColor: const Color.fromARGB(255, 15, 171, 255),
+              icon: Icon(
+                color: Colors.black26,
+                Icons.monetization_on,
+              ),
+              label: "Currencies"),
+          BottomNavigationBarItem(
+              backgroundColor: const Color.fromARGB(255, 15, 171, 255),
+              icon: Icon(
+                color: Colors.black26,
+                Icons.monetization_on,
+              ),
+              label: "History"),
+          BottomNavigationBarItem(
+              backgroundColor: const Color.fromARGB(255, 15, 171, 255),
+              icon: Icon(
+                color: Colors.black26,
+                Icons.monetization_on,
+              ),
+              label: "User"),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:currency_converter_app/Screens/Converter_Screen.dart';
 import 'package:currency_converter_app/Screens/Login_screen.dart';
 import 'package:currency_converter_app/Screens/SingIn_screen.dart';
+import 'package:currency_converter_app/Screens/main_page.dart';
 import 'package:flutter/material.dart';
 
 import 'Screens/CurrencyList.dart';
@@ -9,13 +10,17 @@ void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: 'ConverterPage',
-      routes: {
-        'login': (context) => LoginScreen(),
-        'singIn': (context) => SingIn(),
-        'currencyList': (context) => CurrencyList(),
-        'ConverterPage': (context) => ConverterScreen(),
-      },
+      title: "Flutter currency app",
+      home: MainPage(),
+
+      // bcz we use buttom navigation here
+      // initialRoute: 'ConverterPage',
+      // routes: {
+      //   'login': (context) => LoginScreen(),
+      //   'singIn': (context) => SingIn(),
+      //   'currencyList': (context) => CurrencyList(),
+      //   'ConverterPage': (context) => ConverterScreen(),
+      // },
     ),
   );
 }
